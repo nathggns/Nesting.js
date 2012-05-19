06f301ec9141cdb3c6e1cd34636bff5a914a0a32
Nathaniel Higgins <nat@nath.is>
2012-05-19 13:22:45 +0100

    Revert "Fix test to use files from build_config"
    This reverts commit c42b7ea983b863b5e20f9dc5eb69c8f23ae8e2b6.

--- test/scripts.php
+++ test/scripts.php
@@ -1,6 +1,13 @@
 <?php
 header("Content-type: text/javascript");
-$config = json_decode(file_get_contents(("../build_config.json")), true);
-$files = $config['files'];
+$files = array(
+	"intro.js",
+	"Modernizr.js",
+	"support.js",
+	"parser.js",
+	"normalise.js",
+	"nesting.js",
+	"outro.js"
+);
 
 foreach ($files as $file) echo file_get_contents("../src/$file");
